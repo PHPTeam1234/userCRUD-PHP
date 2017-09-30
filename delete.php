622aa8ebb6d3fc49ac1ede59df9a6bc040ab6a33
lancelotpro <lancelot.pro@outlook.com>
2017-09-30 10:04:39 +0800

    upload for jingan
    change dabase username and password

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@
 	}
 
 	//连接mysql数据库
-	$pdo = new PDO('mysql:host=localhost;dbname=php','root','mysql');
+	$pdo = new PDO('mysql:host=ns2m2jkl.2106.dnstoo.com;port=4010;dbname=phptest1234','phptest1234_f','mysql1234');
 	//设置提交的编码格式
 	$pdo -> exec('set names utf8');
 	//查询所用到的sql语句
